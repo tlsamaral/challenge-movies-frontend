e83557593549910f4ab9efc243a438de9cb9682f
talles.amaral <worktalles@hotmail.com>
2024-08-14 08:47:39 -0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FooterContainer, FooterContent, FooterText } from "./style";
+
+export default function Footer() {
+    return (
+        <FooterContainer>
+            <FooterContent>
+                <FooterText>&copy; 2024 Rader. All rights reserved</FooterText>
+            </FooterContent>
+        </FooterContainer>
+    )
+}
